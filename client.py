ef31a1c96f5ecb13d18623761d330d1bd4537ee6
safarie <timmiedema@icloud.com>
2020-10-28 16:43:04 +0100

    heb de loop voor stoplichten gemaakt

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+import json
+
+host = "127.0.0.1"
+port = 54000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+data = client.recv(1024)
+print(json.loads(data.decode("utf-8")))

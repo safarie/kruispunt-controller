b7b90707a0b5b4900815677ca3731a21979e703e
OutForSomeCode <43600545+OutForSomeCode@users.noreply.github.com>
2020-10-30 13:09:30 +0100

    Add files via upload

---
+++ Controller.py
@@ -0,0 +1,31 @@
+import socket
+import json
+import time
+
+from Functions import *
+
+host = "127.0.0.1"
+port = 54000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(5)
+
+print("waiting for client")
+
+go = True
+stage = 1
+
+while True:
+	client, addr = server.accept()
+	while go:
+		updateLight(stage)
+		size = len(str(data))
+		client.send(bytes(str(size) + ":", "utf-8") + bytes(json.dumps(data), "utf-8"))
+		time.sleep(15)
+		if stage >= 4:
+			stage = 1
+		else:
+			stage += 1
+	else:
+		client.close()
